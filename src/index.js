7b621e8080c676ab6ca71dff1fa9bd9d946dc488
Heli Ribeiro <heliribeiro2@gmail.com>
2021-09-26 16:43:32 -0300

    Api passando em todos os testes.

--- src/index.js
+++ src/index.js
@@ -14,14 +14,19 @@ function checksExistsUserAccount(request, response, next) {
   const {username} = request.headers
   const user = users.find(user => user.username === username)
   if(!user) {
-    return response.json({error: 'User does not exists.'})
+    return response.status(404).json({	error: 'Mensagem do erro'})
   }
+  request.user = user
   next()
 }
 
 app.post('/users', (request, response) => {
   // Complete aqui
   const {name, username } = request.body
+  const userExists = users.find(user => user.username === username)
+  if(userExists) {
+    return response.status(400).json({	error: 'Mensagem do erro'})
+  } 
   const user = {
     id: v4(),
     name,
@@ -29,64 +34,72 @@ app.post('/users', (request, response) => {
     todos: []
   }
   users.push(user)
-  response.status(200).send(user)
+  response.status(201).send(user)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
-  const {username} = request.headers
-  const todos = users.find(user => user.username === username).todos
-  response.json(todos)
+  const {user} = request
+  response.json(user.todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
-   const {username} = request.headers
+
    const {title, deadline } = request.body
-   const user = users.find(user => user.username === username)
+   const {user} = request
+   const id = v4()
   user.todos.push({
-    id: v4(),
+    id,
     title,
     done: false,
     deadline: new Date(deadline),
     created_at: new Date()
   })
-  return response.json(user)
+   let todo = user.todos.find(todo => todo.id === id)
+  return response.status(201).json(todo)
    
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   // Complete aqui
-  const {username} = request.headers
+ 
   const {title, deadline} = request.body
   const {id} = request.params
-  const user = users.find(user => user.username === username)
+  const {user} = request
 
   // encontra a tarefa a ser alterada
   let todo = user.todos.find(todo => todo.id === id)
+  if(!todo) {
+    return response.status(404).json({error: 'Mensagem de erro'})
+  }
   todo = {...todo, title, deadline}
 
   //Encontra o indice da tarefa, remove a tarefa antiga e adiciona a atual.
   let index  = user.todos.findIndex(todo=> todo.id === id)
   user.todos.splice(index,1, todo)
 
-  return response.json(todo)
+  return response.status(200).json(todo)
 
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
   // Complete aqui
-  const {username} = request.headers
+  const {user} = request
   const {id} = request.params
-  const user = users.find(user => user.username === username)
+  
   let todo = user.todos.find(todo => todo.id === id)
+  
+  if(!todo) {
+    return response.status(404).json({error: 'Mensagem de erro'})
+  }
   todo = {...todo, done:true}
 
   //Encontra o indice da tarefa, remove a tarefa antiga e adiciona a atual.
   let index  = user.todos.findIndex(todo=> todo.id === id)
   user.todos.splice(index,1, todo)
 
-  return response.json(todo)
+  return response.status(200).json(todo)
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
@@ -97,9 +110,12 @@ app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
 
   //Encontra o indice da tarefa e remove a tarefa.
   let index  = user.todos.findIndex(todo=> todo.id === id)
+  if(index === -1) {
+    return response.status(404).json({error: 'Mensagem de erro'})
+  }
   user.todos.splice(index,1)
 
-  return response.send(200)
+  return response.status(204).send()
 
 });
 
