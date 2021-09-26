0cc63537f298b946e3bef6cf847ba8b4c5066bab
Heli Ribeiro <heliribeiro2@gmail.com>
2021-09-26 15:22:40 -0300

    Finalizadas as rotas de criação de usuario e de todo, de listagem de todos, e de atualização.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,77 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4, v4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const {username} = request.headers
+  const user = users.find(user => user.username === username)
+  if(!user) {
+    return response.json({error: 'User does not exists.'})
+  }
+  next()
 }
 
 app.post('/users', (request, response) => {
   // Complete aqui
+  const {name, username } = request.body
+  const user = {
+    id: v4(),
+    name,
+    username,
+    todos: []
+  }
+  users.push(user)
+  response.status(200).send(user)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const {username} = request.headers
+  const todos = users.find(user => user.username === username).todos
+  response.json(todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+   const {username} = request.headers
+   const {title, deadline } = request.body
+   const user = users.find(user => user.username === username)
+  user.todos.push({
+    id: v4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  })
+  return response.json(user)
+   
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const {username} = request.headers
+  const {title, deadline} = request.body
+  const {id} = request.params
+  const user = users.find(user => user.username === username)
+
+  // encontra a tarefa a ser alterada
+  let todo = user.todos.find(todo => todo.id === id)
+  todo = {...todo, title, deadline}
+
+  //Encontra o indice da tarefa, remove a tarefa antiga e adiciona a atual.
+  let index  = user.todos.findIndex(todo=> todo.id === id)
+  user.todos.splice(index,1, todo)
+
+  return response.json(todo)
+
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
